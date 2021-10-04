01ca949a7b9377b3fed3c62ad5a2a97f30291a63
yulaseo <yula0821@naver.com>
2021-10-04 19:38:25 +0900

    fix: add diary
    http://localhost:8000/diary/new/ 에서 diary 작성 시 DB에 추가

--- diary/models.py
+++ diary/models.py
@@ -4,9 +4,9 @@ from django.utils import timezone
 
 
 class Diary(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    content = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
     # hashtag
--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Diary
+from .forms import DiaryForm
+
+from django.utils import timezone
 
 
 def calender(request):
@@ -10,13 +14,32 @@ def search(request):
     return HttpResponse("Search index.")
 
 
-def add(request):
-    return render(request, 'diary/add_diary.html')
+def new(request):
+    if request.method == 'POST':
+        form = DiaryForm(request.POST)
+        if form.is_valid():
+            diary = form.save(commit=False)
+            diary.user = request.user
+            diary.created_date = timezone.now()
+            diary.save()
+            # return redirect('detail', diary_id=diary.id)
+    else:
+        form = DiaryForm()
+
+    return render(request, 'diary/new_diary.html', {'form': form})
 
 
 def detail(request, diary_id):
-    return HttpResponse("Detail %s." % diary_id)
+    diary = Diary.objects.get(id=diary_id)
+    context = {
+        'diary': diary,
+    }
+    return render(request, 'diary/diary_detail.html', context)
 
 
 def edit(request, diary_id):
     return HttpResponse("Edit %s." % diary_id)
+
+
+def diary_list(request):
+    return HttpResponse("List %s.")
