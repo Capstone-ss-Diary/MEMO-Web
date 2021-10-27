64631fbf5d0ac9c55a1b40aa5b303c2a4fdd80f3
Sohyeon <thgus345@naver.com>
2021-10-27 17:41:25 +0900

    Diary 모델 수정

--- diary/admin.py
+++ diary/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
+from django.db import models
 from .models import Diary, DiaryText, DiaryImage
 
-admin.site.register(Diary)
-admin.site.register(DiaryText)
-admin.site.register(DiaryImage)
+
+class DiaryText(admin.TabularInline):
+    model = DiaryText
+
+
+class DiaryImageInline(admin.TabularInline):
+    model = DiaryImage
+
+
+class DiaryAdmin(admin.ModelAdmin):
+    inlines = [
+        DiaryText,
+        DiaryImageInline,
+    ]
+
+
+admin.site.register(Diary, DiaryAdmin)
+
+
+# admin.site.register(Diary)
+# admin.site.register(DiaryText)
+# admin.site.register(DiaryImage)
 
 # 검색 기능
 # class DiaryAdmin(admin.ModelAdmin):
